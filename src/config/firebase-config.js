50e0d9fe101a8198724b1c52478679217e7d170b
andrewconstancio <61609406+andrewconstancio@users.noreply.github.com>
2022-03-22 19:09:12 -0500

    com
    com

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -4,25 +4,17 @@ import  'firebase/compat/firestore';
 import { getFirestore }  from '@firebase/firestore';
 import 'firebase/compat/storage'; 
 
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
-// };
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7QPGZ4bly5okSQ1BAmj6eC5gurBp53MU",
-    authDomain: "neetbook-71cb0.firebaseapp.com",
-    projectId: "neetbook-71cb0",
-    storageBucket: "neetbook-71cb0.appspot.com",
-    messagingSenderId: "51835178688",
-    appId: "1:51835178688:web:0b4536d1682160dc6e2725",
-    measurementId: "G-YVF0SS6GEV"
-  };
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID  
+};
 
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
